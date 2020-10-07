972b3e27974e4d23768119767078d4803afb6d93
Simo RAMI <sr-_-7@hotmail.com>
2020-10-07 16:19:24 +0100

    new update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended:true} ));
 app.use(cors());
 
-if(process.env.NODE_ENV ==='production') {
+if(process.env.NODE_ENV =='production') {
     app.use(express.static(path.join(__dirname,'client/build')));
 
     app.get('*',function(req,res){
