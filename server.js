a415d78571836d37d1afe6c3662658eed5da024f
Simo RAMI <sr-_-7@hotmail.com>
2020-10-07 16:58:52 +0100

    new

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended:true} ));
 app.use(cors());
 
-if(process.env.NODE_ENV =='production') {
+if(process.env.NODE_ENV ==='production') {
     app.use(express.static(path.join(__dirname,'client/build')));
 
     app.get('*',function(req,res){
@@ -24,7 +24,7 @@ if(process.env.NODE_ENV =='production') {
 
 app.listen(port,error =>{
     if(error) throw error;
-    console.log('Server running on port :'+port);
+    console.log('Server running on port '+port);
 });
 
 app.post('/payment', (req, res) => {
